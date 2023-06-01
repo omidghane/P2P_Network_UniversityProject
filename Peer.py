fd2dcb90660be8595758d2ed9d2e76c156e9e5b2
Omid Ghane <omidjt2015@gmail.com>
2023-06-01 06:29:01 +0330

    Update Peer.py
    added feature: media transporting

--- Peer.py
+++ Peer.py
@@ -1,16 +1,22 @@
 import json
+import random
 import socket
 import threading
 
+from PIL import Image
+from PIL import *
+import numpy as np
 import requests
 
 MYPORT = 7447
 MYNAME = "omid"
-MESSAGE_SIZE = 64
+MESSAGE_SIZE = 400
 ENCODING = 'utf-8'
 
 peer_address = (0, 0)
 accepted_request = False
+accepted_request2 = False
+accepted_request3 = False
 
 STATE = 0
 
@@ -21,8 +27,10 @@ def main():
     global MYNAME
     # global peer_address
     address = socket.gethostbyname(socket.gethostname())
-    MYPORT = int(input("your_port: "))
+    # MYPORT = int(input("your_port: "))
+    MYPORT = random.randint(1,1000)
     MYNAME = input("your_name: ")
+    # MYNAME = "qw"
     host = (address, MYPORT)
     print(host, "MyPort")
 
@@ -38,6 +46,7 @@ def main():
     while (True):
         STATE = 0
         inp = input("1)all_name_users 2)communicate\n")
+        # inp = 2
         if int(inp) == 1:
             namePeers_GET()
         elif int(inp) == 2:
@@ -111,21 +120,25 @@ def find_peerAddress(username):
 def communicate(server):
     global STATE
     global peer_address
+    global accepted_request2
     # global accepted_request
+    global accepted_request3
     while True:
         STATE = 1
         con = input("send message? (y/n): ")
+        # con = "y"
         if con == "y":
             break
 
     if not accepted_request:
         userName = input("User_name: ")
+        # userName = "as"
         peer_address = find_peerAddress(userName)
-        print(peer_address, " peeradd")
+        # print(peer_address, " peeradd")
         if not accepted_request:
             # peer_address = (socket.gethostbyname(socket.gethostname()), port)
             my_mess = "REQUEST"
-            res=send_message(server, my_mess, peer_address)
+            res = send_message(server, my_mess, peer_address, 1)
             if res == "error":
                 return
             # print('request sent')
@@ -133,24 +146,50 @@ def communicate(server):
                 pass
 
     while True:
-        STATE = 3
-        my_mess = input("you: ")
+        STATE = 5
+        choose = input("Data to send: 1)text 2)img\n")
+        choose = int(choose)
         try:
-            send_message(server, my_mess, peer_address)
+            if choose == 1:
+                res = send_message(server, "TEXT", peer_address, 1)
+                while not accepted_request2:
+                    # print("i waitttt")
+                    pass
+                STATE = 3
+                my_mess = input("you: ")
+                send_message_tcp(my_mess, peer_address)
+            elif choose == 2:
+                res = send_message(server, "IMG", peer_address, 1)
+                while not accepted_request3:
+                    # print("i waitttt")
+                    pass
+                # STATE = 6
+            else:
+                print("Invalid option")
         except:
             print("Error: Failed to send message. Please check your connection.")
             continue
 
 
-def send_message(client, msg, server_address):
-    message = msg.encode(ENCODING)
-
-    msg_length = len(message)
-    msg_length = str(msg_length).encode(ENCODING)
-    msg_length += b' ' * (MESSAGE_SIZE - len(msg_length))
+def send_message(client, msg, server_address, mode):
+    if mode == 1:
+        message = msg.encode(ENCODING)
+    else:
+        message = msg
 
     try:
         client.sendto(message, server_address)
+    except:
+        print("(FUNC) Error: Failed to send message. Please check your connection.")
+        return "error"
+
+
+def send_message_tcp(msg, server_address):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(server_address)
+        client_socket.sendall(msg.encode(ENCODING))
+        client_socket.close()
     except:
         print("Error: Failed to send message. Please check your connection.")
         return "error"
@@ -160,6 +199,8 @@ def receive_messages(server, nothing):
     global STATE
     global accepted_request
     global peer_address
+    global accepted_request2
+    global accepted_request3
     acceptCommunication = False
     while (True):
 
@@ -176,32 +217,150 @@ def receive_messages(server, nothing):
             # answer = input("do you accept {} (y,n)? ".format(address[1]))
             answer = 'y'
             if answer == 'y':
-                send_message(server, "ACCEPTED", address)
+                send_message(server, "ACCEPTED", address, 1)
                 accepted_request = True
                 acceptCommunication = True
                 peer_address = address
+                # print("you accepted the request")
             else:
-                send_message(server, "REJECTED", address)
+                send_message(server, "REJECTED", address, 1)
             # print('accepted')
 
         # if not acceptCommunication:
         while acceptCommunication:
             try:
                 message, address = server.recvfrom(MESSAGE_SIZE)
+                # print("new message: ", message)
+                msg = message.decode(ENCODING)
+                if msg == "TEXT":
+                    # print("wait for text zzzzzz")
+                    send_message(server, "ACCEPTED_TEXT", address, 1)
+                    receive_messages_tcp(server, None)
+                elif msg == "IMG":
+                    send_message(server, "ACCEPTED_IMG", address, 1)
+                    # message, address = server.recvfrom(MESSAGE_SIZE)
+                    receive_image_udp(server)
+                    print("I accepted")
+                elif msg == "ACCEPTED_TEXT":
+                    # print("he accepted the text")
+                    accepted_request2 = True
+                    continue
+                elif msg == "ACCEPTED_IMG":
+                    img_path = "r2zzMxBl_400x400.jpg"
+                    img_matrix, width, height = image_to_matrix(img_path)
+                    # print(img_matrix, " img_matrix")
+                    send_image_udp(server, img_matrix, width, height, peer_address)
+                    accepted_request3 = True
+                    print("he accepted")
             except:
-                print("Error: Failed to receive message. Please check your connection.")
+                print("UDP_Error: Failed to receive message. Please check your connection.")
                 continue
-            print("\nConnection is started from {}".format(address))
             msg = message.decode(ENCODING)
-            print("Message: {}".format(msg))
+            # print("Message: {}".format(msg))
             if STATE == 0:
                 print("1)all_name_users 2)communicate")
+                pass
             elif STATE == 1:
                 print("send message? (y/n): ", end='')
-            else:
+                pass
+            elif STATE == 4:
                 print("you: ", end='')
-            # if msg == "DISCONNECTED":
-            #     server.close()
+            else:
+                print("Data to send: 1)text 2)img")
+
+
+def receive_messages_tcp(server, nothing):
+    global STATE
+    global accepted_request
+    global peer_address
+    global MYPORT
+
+    try:
+        tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_socket.bind((socket.gethostbyname(socket.gethostname()), MYPORT))
+        tcp_socket.listen(1)
+        # client_socket.connect(server_address)
+        tcp_socket, client_address = tcp_socket.accept()
+
+        msg = tcp_socket.recv(MESSAGE_SIZE).decode(ENCODING)
+    except:
+        print("Error: Failed to send message. Please check your connection.")
+        return "error"
+    # client_socket, client_address = server.accept()
+    print("Message: {}".format(msg))
+    tcp_socket.close()
+
+from numpy import asarray
+def image_to_matrix(image_path):
+
+    try:
+        image = Image.open(image_path)
+        width, height = image.size  # Get the image size
+        # matrix = image.convert("L").getdata()
+        matrix = asarray(image)
+        return matrix, width, height
+    except Exception as e:
+        print("Error: Failed to convert image to matrix.")
+        print(e)
+        return None
+
+
+def receive_image_udp(server):
+    message, address = server.recvfrom(MESSAGE_SIZE)
+    # matrix_width = int(message.decode(ENCODING))
+    m = message.decode(ENCODING)
+    matrix_width, matrix_height = m.split("_")
+    matrix_width, matrix_height = int(matrix_width), int(matrix_height)
+    # print("size received qqqqqqq")
+
+    received_data = b""
+    # print(matrix_width * MESSAGE_SIZE, " amount ot receive eeeeeee")
+    matrix = np.empty((matrix_width, matrix_height, 3), dtype=np.uint8)
+
+    for i in range(matrix_width):
+        print(i)
+        chunk_data = b""
+        while len(chunk_data) < matrix_height * 3:  # Assuming each pixel has 3 bytes (RGB)
+            message, address = server.recvfrom(MESSAGE_SIZE)
+            chunk_data += message
+            send_message(server, "ACK", address, 1)
+
+        chunk_array = np.frombuffer(chunk_data, dtype=np.uint8).reshape((matrix_height, 3))
+        matrix[i] = chunk_array
+
+        # received_data += chunk_data
+
+        # chunk_data = np.frombuffer(received_data, dtype=np.uint8)
+        # matrix[i] = chunk_data.reshape(matrix_width, -1)
+
+
+    try:
+        from PIL import Image
+        image = Image.fromarray(matrix)
+        image.save("received_image.jpg")
+        image.show()
+    except ImportError:
+        print("Error: Pillow library is required to process the received image.")
+        return
+
+
+def send_image_udp(client, matrix, width, height, server_address):
+    # matrix_width = width
+    send_message(client, str(f"{width}_{height}"), server_address, 1)
+
+    for i in range(0, width):
+        for j in range(0, height, int(MESSAGE_SIZE/3)):
+            if j+int(MESSAGE_SIZE/3) > height:
+                chunk = matrix[i][j:height]
+            else:
+                chunk = matrix[i][j:j + int(MESSAGE_SIZE/3)]
+            chunk_bytes = chunk.tobytes()  # Convert the NumPy array to bytes
+            send_message(client, chunk_bytes, server_address, 2)
+            response, _ = client.recvfrom(MESSAGE_SIZE)
+            if response.decode(ENCODING) != "ACK":
+                print("Error: Failed to send image data.")
+                return
+
 
 
 if __name__ == '__main__':
